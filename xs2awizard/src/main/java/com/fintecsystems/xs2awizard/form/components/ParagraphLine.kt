9b55d1d0a9961d27a263d5c5b55ad750bc7c6c43
Maik Mursall <mmursall@fintecsystems.com>
2021-10-06 15:01:50 +0200

    Improved styling api

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/ParagraphLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/ParagraphLine.kt
@@ -28,18 +28,18 @@ class ParagraphLine : FormLine() {
 
                 val backgroundColor = themedValues.getColor(
                     when (formData.severity) {
-                        "info" -> R.styleable.XS2ATheme_paragraph_info_background_color
-                        "error" -> R.styleable.XS2ATheme_paragraph_error_background_color
-                        "warning" -> R.styleable.XS2ATheme_paragraph_warning_background_color
-                        else -> R.styleable.XS2ATheme_paragraph_background_color
+                        "info" -> R.styleable.XS2ATheme_xs2a_paragraph_info_background_color
+                        "error" -> R.styleable.XS2ATheme_xs2a_paragraph_error_background_color
+                        "warning" -> R.styleable.XS2ATheme_xs2a_paragraph_warning_background_color
+                        else -> R.styleable.XS2ATheme_xs2a_paragraph_background_color
                     }, 0
                 )
                 val textColor = themedValues.getColor(
                     when (formData.severity) {
-                        "info" -> R.styleable.XS2ATheme_paragraph_info_text_color
-                        "error" -> R.styleable.XS2ATheme_paragraph_error_text_color
-                        "warning" -> R.styleable.XS2ATheme_paragraph_warning_text_color
-                        else -> R.styleable.XS2ATheme_paragraph_text_color
+                        "info" -> R.styleable.XS2ATheme_xs2a_paragraph_info_text_color
+                        "error" -> R.styleable.XS2ATheme_xs2a_paragraph_error_text_color
+                        "warning" -> R.styleable.XS2ATheme_xs2a_paragraph_warning_text_color
+                        else -> R.styleable.XS2ATheme_xs2a_paragraph_text_color
                     }, 0
                 )
 
