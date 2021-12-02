91c7fc75bd8d23531f08dd92a49a7a03e09e1539
Maik Mursall <mmursall@fintecsystems.com>
2021-12-02 18:05:01 +0100

    Removed unused Methods

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/helper/Utils.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/helper/Utils.kt
@@ -7,36 +7,12 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
-import android.view.LayoutInflater
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.appcompat.view.ContextThemeWrapper
 import com.fintecsystems.xs2awizard.R
-import java.util.concurrent.atomic.AtomicInteger
 
 
 object Utils {
-    private val sNextGeneratedId: AtomicInteger = AtomicInteger(1)
-
-    // https://peacecodemonk.wordpress.com/2016/01/12/generating-id-for-view-elements-programatically-for-android/
-    /**
-     * Generate a value suitable for use in [.setId].
-     * This value will not collide with ID values generated at build time by aapt for R.id.
-     *
-     * @return a generated ID value
-     */
-    fun generateViewId(): Int {
-        while (true) {
-            val result: Int = sNextGeneratedId.get()
-            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
-            var newValue = result + 1
-            if (newValue > 0x00FFFFFF) newValue = 1 // Roll over to 1, not 0.
-            if (sNextGeneratedId.compareAndSet(result, newValue)) {
-                return result
-            }
-        }
-    }
-
     /**
      * Decode Base64-String into Bitmap
      *
@@ -71,44 +47,6 @@ object Utils {
             )
         }
     }
-
-    /**
-     * Clones provided inflater and applies provided theme onto it.
-     *
-     * @param inflater to be cloned inflater.
-     * @param context context used for retrieving theme.
-     * @param styleRes id to the style res.
-     *
-     * @return the themed layout inflater.
-     */
-    fun getThemedInflater(
-        inflater: LayoutInflater,
-        context: Context,
-        styleRes: Int?,
-        styleBaseRes: Int = R.style.XS2ATheme_Base
-    ): LayoutInflater = inflater.cloneInContext(
-        getThemedContext(context, styleRes, styleBaseRes)
-    )
-
-    /**
-     * Wraps context with theme
-     *
-     * @param context context used for retrieving theme.
-     * @param styleRes id to the style res.
-     * @param styleBaseRes id to the base style res.
-     *
-     * @return the themed context.
-     */
-    fun getThemedContext(
-        context: Context,
-        styleRes: Int?,
-        styleBaseRes: Int = R.style.XS2ATheme_Base
-    ): Context =
-        ContextThemeWrapper(context, styleBaseRes).apply {
-            if (styleRes != null) {
-                theme.applyStyle(styleRes, true)
-            }
-        }
 
     /**
      * Copies the supplied text to the clipboard.
