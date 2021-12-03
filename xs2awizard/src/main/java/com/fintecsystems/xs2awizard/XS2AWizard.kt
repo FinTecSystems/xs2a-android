f10add7d79c3c6e1a7788ddcf9abcfbb6b48083c
Maik Mursall <mmursall@fintecsystems.com>
2021-12-03 12:11:49 +0100

    Added submit and receive methods

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/XS2AWizard.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/XS2AWizard.kt
@@ -1,7 +1,9 @@
 package com.fintecsystems.xs2awizard
 
+import android.app.Application
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,32 +18,188 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.fintecsystems.xs2awizard.components.JSONFormatter
 import com.fintecsystems.xs2awizard.components.XS2AWizardConfig
 import com.fintecsystems.xs2awizard.components.XS2AWizardError
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
-import com.fintecsystems.xs2awizard.form.FormLineData
-import com.fintecsystems.xs2awizard.form.FormResponse
-import com.fintecsystems.xs2awizard.form.ParagraphLineData
+import com.fintecsystems.xs2awizard.form.*
 import com.fintecsystems.xs2awizard.form.components.ParagraphLine
+import com.fintecsystems.xs2awizard.helper.Utils
 import com.fintecsystems.xs2awizard_networking.NetworkingInstance
-import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.*
 
-class XS2AWizardViewModel : ViewModel() {
-    private lateinit var networkingInstance: NetworkingInstance
+private const val TAG = "XS2AWizard"
+
+class XS2AWizardViewModel(application: Application) : AndroidViewModel(application) {
     private lateinit var config: XS2AWizardConfig
 
-    val form = MutableLiveData<List<FormLineData>>()
+    val form = MutableLiveData<MutableList<FormLineData>>()
 
-    fun onStart(context: Context, _config: XS2AWizardConfig) {
-        networkingInstance = NetworkingInstance.getInstance(context)
+    private val context: Context
+        get() = getApplication<Application>().applicationContext
+
+    fun onStart(_config: XS2AWizardConfig) {
         config = _config
 
-        networkingInstance.apply {
+        NetworkingInstance.getInstance(context).apply {
             sessionKey = config.sessionKey
             backendURL = config.backendURL
+        }
+
+        initForm()
+    }
+
+    /**
+     * Initializes the Form
+     *
+     * This function does the very first API call to XS2A
+     * and gets a form to render as response.
+     */
+    private fun initForm() = submitForm(
+        buildJsonObject {
+            put("version", JsonPrimitive(BuildConfig.VERSION))
+            put("client", JsonPrimitive(context.getString(R.string.xs2a_client_tag)))
+        },
+        true
+    )
+
+    /**
+     * Construct the JSON body for the form request to the backend.
+     *
+     * @param action [String] that describes the action of the form request.
+     * @param values [JsonObject] used to override fields of request body.
+     *
+     * @return built [JsonObject]
+     */
+    private fun constructJsonBody(action: String, values: JsonObject? = null) = buildJsonObject {
+        form.value?.forEach { appendValueToJson(this, it) }
+
+        put("action", JsonPrimitive(action))
+
+        values?.entries?.forEach {
+            put(it.key, it.value)
+        }
+    }
+
+    /**
+     * Append data of formLine to the resulting jsonObject.
+     * Used for recursion purposes on MultiLine.
+     *
+     * @param jsonBuilder current [JsonBuilder] instance.
+     * @param formLine [FormLineData] element used for appending it's data to [JsonBuilder].
+     */
+    private fun appendValueToJson(jsonBuilder: JsonObjectBuilder, formLine: FormLineData) {
+        when (formLine) {
+            is ValueFormLineData -> jsonBuilder.put(formLine.name, formLine.value ?: JsonNull)
+            is MultiLineData -> {
+                jsonBuilder.put(formLine.name, formLine.selected)
+
+                /* TODO: Find a solution to this
+                (formLine as MultiLine).getCurrentForm()
+                    ?.forEach { appendValueToJson(jsonBuilder, it) }
+                 */
+            }
+        }
+    }
+
+    /**
+     * Submits form using standard "submit" action.
+     */
+    fun submitForm(): Unit = submitForm("submit")
+
+    /**
+     * Constructs request body and submits form using the specified action.
+     *
+     * @param action action to use.
+     */
+    fun submitForm(action: String): Unit = submitForm(constructJsonBody(action), true)
+
+    /**
+     * Submits form using the specified request body.
+     *
+     * @param jsonBody request body.
+     * @param showIndicator show loading indicator during request.
+     */
+    fun submitForm(jsonBody: JsonElement, showIndicator: Boolean) =
+        submitForm(jsonBody.toString(), showIndicator)
+
+    /**
+     * Submits form using the specified request body.
+     *
+     * @param jsonBody stringified request body.
+     * @param showIndicator show loading indicator during request.
+     */
+    fun submitForm(jsonBody: String, showIndicator: Boolean) {
+        /* TODO: Add again, when loading indicator exists
+        if (showIndicator) {
+            incrementLoadingIndicatorLock()
+        }
+         */
+
+        // TODO: Find a fix
+        // Utils.hideSoftKeyboard(activity)
+
+        return NetworkingInstance.getInstance(context)
+            .encodeAndSendMessage(
+                jsonBody,
+                ::onFormReceived
+            )
+    }
+
+    /**
+     * Parses form response and possibly re-/builds the form.
+     *
+     * @param jsonString form response as a JSON [String].
+     */
+    private fun onFormReceived(jsonString: String) {
+        try {
+            Log.d(TAG, "onFormReceived: $jsonString")
+
+            /* TODO
+            // Postpone until we're back into foreground.
+            if (isInBackground) {
+                onFormReceivedDataFromBackground = jsonString
+
+                return
+            }
+             */
+
+            val formResponse = JSONFormatter.formatter.decodeFromString<FormResponse>(jsonString)
+
+            /* TODO
+            // Check if we're in the right language. If not change it.
+            if (checkIfLanguageNeedsToBeChanged(formResponse.language)) {
+                Log.d(
+                    TAG,
+                    "onFormReceived: Language is not the same! Form: ${formResponse.language} Device: ${Locale.getDefault().language}, Changing!"
+                )
+
+                submitForm(
+                    buildJsonObject {
+                        put("action", "change-language")
+                        put("language", Locale.getDefault().language)
+                    }
+                )
+
+                decrementLoadingIndicatorLock()
+
+                return
+            }
+             */
+
+            form.value = formResponse.form?.toMutableList()
+
+            // TODO: Maybe add this again
+            // decrementLoadingIndicatorLock()
+
+            parseCallback(formResponse)
+        } catch (serializationException: SerializationException) {
+            Log.e(TAG, "onFormReceived: $serializationException")
         }
     }
 
@@ -75,10 +233,8 @@ fun XS2AWizardComponent(
 ) {
     val form by xs2aWizardViewModel.form.observeAsState(null)
 
-    val localContext = LocalContext.current
-
     DisposableEffect(xs2aWizardViewModel) {
-        xs2aWizardViewModel.onStart(localContext, xS2AWizardConfig)
+        xs2aWizardViewModel.onStart(xS2AWizardConfig)
 
         onDispose { }
     }
