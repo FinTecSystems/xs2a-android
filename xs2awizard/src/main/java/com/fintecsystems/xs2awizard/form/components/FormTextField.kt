d243e6c38d9cde87f2965eebb9f3681ecb51518e
Maik Mursall <mmursall@fintecsystems.com>
2021-12-03 17:39:52 +0100

    Added more props to FormTextField

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/FormTextField.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/FormTextField.kt
@@ -1,20 +1,38 @@
 package com.fintecsystems.xs2awizard.form.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
 
 @Composable
 fun FormTextField(
     value: TextFieldValue,
     onValueChange: (TextFieldValue) -> Unit,
+    placeholder: String?,
+    enabled: Boolean = true,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
 ) {
     TextField(
         value = value,
+        enabled = enabled,
+        placeholder = {
+            if (placeholder != null)
+                Text(
+                    text = placeholder,
+                    style = TextStyle(
+                        color = XS2ATheme.CURRENT.textColor,
+                    )
+                )
+        },
         onValueChange = onValueChange,
         shape = XS2ATheme.CURRENT.inputShape,
         modifier = Modifier.fillMaxWidth(),
@@ -22,5 +40,7 @@ fun FormTextField(
             textColor = XS2ATheme.CURRENT.inputTextColor,
             backgroundColor = XS2ATheme.CURRENT.inputBackgroundColor
         ),
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
     )
 }
