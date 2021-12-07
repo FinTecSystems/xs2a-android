8a7915529805fe990b66dc6257ced30334c772a4
Maik Mursall <mmursall@fintecsystems.com>
2021-12-07 13:53:45 +0100

    Added Spacing to Buttons

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/RedirectLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/RedirectLine.kt
@@ -1,16 +1,21 @@
 package com.fintecsystems.xs2awizard.form.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.fintecsystems.xs2awizard.XS2AWizardViewModel
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
 import com.fintecsystems.xs2awizard.form.RedirectLineData
 
 @Composable
 fun RedirectLine(formData: RedirectLineData, viewModel: XS2AWizardViewModel) {
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
         FormButton(label = formData.label!!, buttonStyle = XS2ATheme.CURRENT.redirectButtonStyle) {
             viewModel.openWebView(formData.url!!)
         }
