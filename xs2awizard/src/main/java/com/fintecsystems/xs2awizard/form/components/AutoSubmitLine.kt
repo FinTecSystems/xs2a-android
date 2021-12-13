efb3a7cc7fd1f34b49acca75ad3eb732cf672a3b
Maik Mursall <mmursall@fintecsystems.com>
2021-12-13 15:01:53 +0100

    Now using LaunchedEffect for AutoSubmitLine

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/AutoSubmitLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/AutoSubmitLine.kt
@@ -1,8 +1,7 @@
 package com.fintecsystems.xs2awizard.form.components
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.LaunchedEffect
 import com.fintecsystems.xs2awizard.XS2AWizardViewModel
 import com.fintecsystems.xs2awizard.form.AutoSubmitLineData
 import kotlinx.coroutines.delay
@@ -10,15 +9,11 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun AutoSubmitLine(formData: AutoSubmitLineData, viewModel: XS2AWizardViewModel) {
-    val coroutineScope = rememberCoroutineScope()
-
-    DisposableEffect(null) {
-        coroutineScope.launch {
+    LaunchedEffect(null) {
+        launch {
             delay(formData.interval.toLong())
 
             viewModel.submitForm(viewModel.constructJsonBody("autosubmit"), false)
         }
-
-        onDispose { /* no-op */ }
     }
 }
