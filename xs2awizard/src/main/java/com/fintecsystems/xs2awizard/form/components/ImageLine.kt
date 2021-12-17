d463f1bf0c959197f232c521165988d0a37e02be
Maik Mursall <mmursall@fintecsystems.com>
2021-12-17 13:05:40 +0100

    Improved Image Size

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/ImageLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/ImageLine.kt
@@ -2,11 +2,15 @@ package com.fintecsystems.xs2awizard.form.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.fintecsystems.xs2awizard.R
 import com.fintecsystems.xs2awizard.form.ImageLineData
 import com.fintecsystems.xs2awizard.helper.Utils
@@ -21,8 +25,11 @@ fun ImageLine(formData: ImageLineData) {
         Image(
             bitmap = imageBitmap,
             contentDescription = stringResource(id = R.string.image_description),
+            contentScale = ContentScale.Fit,
+            alignment = Alignment.Center,
             modifier = Modifier
-                .align(Alignment.Center)
+                .defaultMinSize(minHeight = 100.dp)
+                .fillMaxSize()
         )
     }
 }
