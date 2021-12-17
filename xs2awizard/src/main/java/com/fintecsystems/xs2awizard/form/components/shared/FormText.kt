3456b8323640891782d6250d2e2165f1d32a495e
Maik Mursall <mmursall@fintecsystems.com>
2021-12-17 13:45:35 +0100

    Added modifier and textAlign prop

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/shared/FormText.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/shared/FormText.kt
@@ -2,24 +2,30 @@ package com.fintecsystems.xs2awizard.form.components.shared
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
 
 @Composable
 fun FormText(
+    modifier: Modifier = Modifier,
     text: String,
     color: Color = XS2ATheme.CURRENT.textColor,
     fontSize: TextUnit = TextUnit.Unspecified,
     fontWeight: FontWeight? = null,
-    maxLines: Int = Int.MAX_VALUE
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign? = null,
 ) {
     Text(
         text = text,
         color = color,
         fontSize = fontSize,
         fontWeight = fontWeight,
-        maxLines = maxLines
+        maxLines = maxLines,
+        textAlign = textAlign,
+        modifier = modifier
     )
 }
