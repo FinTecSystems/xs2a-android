bd1eeceb02ff2f09aaa15cfb86333d9b39b91f7f
Maik Mursall <mmursall@fintecsystems.com>
2022-01-06 18:37:34 +0100

    Added comments to XS2AWizardConfig

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/components/XS2AWizardConfig.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/components/XS2AWizardConfig.kt
@@ -4,6 +4,23 @@ import com.fintecsystems.xs2awizard.components.theme.IXS2ATheme
 
 /**
  * Config of the Wizard-Instance
+ *
+ * @property sessionKey Session-Key to be used.
+ * @property backendURL URL to the Backend.
+ * @property onFinish Called when the session is finished successfully.
+ *                    When using a XS2A.API session with shared_credentials set to true, the parameter will be
+ *                    the shared credential, null otherwise.
+ *
+ * @property onAbort Called when the user aborted the session.
+ * @property onError Called when a Session-Error occurred.
+ *                   Supplies the Error.
+ * @property onNetworkError Called when a Network-Error occurred.
+ * @property onStep Called when a new Wizard-Step is reached.
+ *                  Supplies the new step, may be null if the step is unknown e.g. on success.
+ * @property onBack Called when the Back-Button of the form is pressed.
+ *                  Supplies the current step at the moment of pressing the button.
+ * @property theme Theme to be used.
+ *                 If null the default Light- or Dark-Theme, depending on the device settings, is used.
  */
 data class XS2AWizardConfig(
     val sessionKey: String,
