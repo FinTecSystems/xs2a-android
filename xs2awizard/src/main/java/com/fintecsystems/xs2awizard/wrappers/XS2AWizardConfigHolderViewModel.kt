56e17a29d1fcf5e5160432fb2d4b5d5a05a22eae
Maik Mursall <mmursall@fintecsystems.com>
2022-03-24 13:08:53 +0100

    Now using compose viewModels instead of fragment viewModels

---
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/wrappers/XS2AWizardConfigHolderViewModel.kt
@@ -0,0 +1,8 @@
+package com.fintecsystems.xs2awizard.wrappers
+
+import androidx.lifecycle.ViewModel
+import com.fintecsystems.xs2awizard.components.XS2AWizardConfig
+
+class XS2AWizardConfigHolderViewModel() : ViewModel() {
+    var xS2AWizardConfig: XS2AWizardConfig? = null
+}
--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/wrappers/XS2AWizardFragment.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/wrappers/XS2AWizardFragment.kt
@@ -1,21 +1,15 @@
 package com.fintecsystems.xs2awizard.wrappers
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.fintecsystems.xs2awizard.XS2AWizard
 import com.fintecsystems.xs2awizard.components.XS2AWizardConfig
-
-class ConfigHolder() : ViewModel() {
-    var xS2AWizardConfig: XS2AWizardConfig? = null
-}
 
 /**
  * Wrapper for the XS2A-Wizard Compose Component
@@ -23,17 +17,8 @@ class ConfigHolder() : ViewModel() {
 class XS2AWizardFragment() : Fragment() {
     private var mConfig: XS2AWizardConfig? = null
 
-    private val configHolder: ConfigHolder by viewModels()
-
     constructor(_xS2AWizardConfig: XS2AWizardConfig) : this() {
         mConfig = _xS2AWizardConfig
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        if (mConfig != null)
-            configHolder.xS2AWizardConfig = mConfig
     }
 
     override fun onCreateView(
@@ -44,11 +29,14 @@ class XS2AWizardFragment() : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 
-            Log.d("XS2AWizard", "onCreateView: ${configHolder.xS2AWizardConfig}")
-
             setContent {
-                if (configHolder.xS2AWizardConfig != null)
-                    XS2AWizard(xS2AWizardConfig = configHolder.xS2AWizardConfig!!)
+                val xS2AWizardConfigHolder = viewModel<XS2AWizardConfigHolderViewModel>()
+
+                if (mConfig != null)
+                    xS2AWizardConfigHolder.xS2AWizardConfig = mConfig
+
+                if (xS2AWizardConfigHolder.xS2AWizardConfig != null)
+                    XS2AWizard(xS2AWizardConfig = xS2AWizardConfigHolder.xS2AWizardConfig!!)
             }
         }
     }
