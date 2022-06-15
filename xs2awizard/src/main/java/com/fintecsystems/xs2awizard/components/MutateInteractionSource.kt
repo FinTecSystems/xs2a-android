801c6d2516088112d00ab144e879692168ad17a8
Maik Mursall <mmursall@fintecsystems.com>
2022-06-15 14:57:43 +0200

    Checkbox text now toggles it's state

---
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/components/MutateInteractionSource.kt
@@ -0,0 +1,53 @@
+package com.fintecsystems.xs2awizard.components
+
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+// https://proandroiddev.com/jetpack-compose-interactionsources-the-ripple-effect-and-you-f451b60fcd37
+fun Modifier.mutateInteractionSource(
+    interactionSource: MutableInteractionSource,
+    enabled: Boolean = true,
+    maxDelayMillis: Long = 1000,
+    minDelayMillis: Long = 5,
+    delayDecayFactor: Float = .20f
+): Modifier = pointerInput(interactionSource, enabled) {
+    forEachGesture {
+        coroutineScope {
+            awaitPointerEventScope {
+                val down = awaitFirstDown(requireUnconsumed = false)
+                // Create a down press interaction
+                val downPress = PressInteraction.Press(down.position)
+                val heldButtonJob = launch {
+                    // Send the press through the interaction source
+                    interactionSource.emit(downPress)
+                    var currentDelayMillis = maxDelayMillis
+                    while (enabled && down.pressed) {
+                        delay(currentDelayMillis)
+                        val nextMillis =
+                            currentDelayMillis - (currentDelayMillis * delayDecayFactor)
+                        currentDelayMillis = nextMillis.toLong().coerceAtLeast(minDelayMillis)
+                    }
+                }
+                val up = waitForUpOrCancellation()
+                heldButtonJob.cancel()
+                // Determine whether a cancel or release occurred, and create the interaction
+                val releaseOrCancel = when (up) {
+                    null -> PressInteraction.Cancel(downPress)
+                    else -> PressInteraction.Release(downPress)
+                }
+                launch {
+                    // Send the result through the interaction source
+                    interactionSource.emit(releaseOrCancel)
+                }
+            }
+        }
+    }
+}
--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/CheckBoxLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/CheckBoxLine.kt
@@ -1,6 +1,8 @@
 package com.fintecsystems.xs2awizard.form.components
 
 import android.app.Activity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
@@ -8,12 +10,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Checkbox
 import androidx.compose.material.CheckboxDefaults
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.fintecsystems.xs2awizard.components.XS2AWizardViewModel
+import com.fintecsystems.xs2awizard.components.mutateInteractionSource
+import com.fintecsystems.xs2awizard.components.theme.NoRippleTheme
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
 import com.fintecsystems.xs2awizard.form.CheckBoxLineData
 import com.fintecsystems.xs2awizard.helper.MarkupParser
@@ -36,6 +42,10 @@ fun CheckBoxLine(formData: CheckBoxLineData, viewModel: XS2AWizardViewModel) {
         )
     }
 
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
     /**
      * Callback for when value of the TextField changed.
      *
@@ -48,25 +58,35 @@ fun CheckBoxLine(formData: CheckBoxLineData, viewModel: XS2AWizardViewModel) {
     }
 
     Row(
-        modifier = Modifier.fillMaxWidth()
-            .offset((-14).dp, 0.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .offset((-14).dp, 0.dp)
+            .clickable(interactionSource, rememberRipple()) {
+                onCheckedChange(!checkBoxValue)
+            },
         ) {
-        Checkbox(
-            checked = checkBoxValue,
-            onCheckedChange = ::onCheckedChange,
-            colors = CheckboxDefaults.colors(
-                checkedColor = XS2ATheme.CURRENT.tintColor,
-                uncheckedColor = XS2ATheme.CURRENT.unselectedColor,
-                checkmarkColor = XS2ATheme.CURRENT.onTintColor
+
+        CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
+            Checkbox(
+                checked = checkBoxValue,
+                onCheckedChange = ::onCheckedChange,
+                interactionSource = interactionSource,
+                colors = CheckboxDefaults.colors(
+                    checkedColor = XS2ATheme.CURRENT.tintColor,
+                    uncheckedColor = XS2ATheme.CURRENT.unselectedColor,
+                    checkmarkColor = XS2ATheme.CURRENT.onTintColor
+                )
             )
-        )
+        }
 
         if (!formData.label.isNullOrEmpty()) {
             val annotatedString = MarkupParser.parseMarkupText(formData.label)
             val activity = LocalContext.current.getActivity<Activity>()
 
             ClickableText(
-                modifier = Modifier.padding(0.dp, 14.dp, 0.dp, 0.dp),
+                modifier = Modifier
+                    .padding(0.dp, 14.dp, 0.dp, 0.dp)
+                    .mutateInteractionSource(interactionSource = interactionSource),
                 text = annotatedString,
                 style = TextStyle(
                     color = XS2ATheme.CURRENT.textColor,
@@ -74,8 +94,10 @@ fun CheckBoxLine(formData: CheckBoxLineData, viewModel: XS2AWizardViewModel) {
                 ),
                 onClick = {
                     annotatedString.getStringAnnotations(it, it)
-                        .firstOrNull()?.let { annotation ->
-                            viewModel.handleAnnotationClick(activity!!, annotation)
+                        .firstOrNull().let { annotation ->
+                            if (annotation != null)
+                                viewModel.handleAnnotationClick(activity!!, annotation)
+                            else onCheckedChange(!checkBoxValue)
                         }
                 }
             )
