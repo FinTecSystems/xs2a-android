2baf261ce8911d7ebb479b44d903cf2f36b09b34
Maik Mursall <mmursall@fintecsystems.com>
2022-08-10 10:59:20 +0200

    MarkupParser is now able to interpret queries in urls

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/helper/MarkupParser.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/helper/MarkupParser.kt
@@ -13,7 +13,7 @@ import kotlinx.serialization.json.buildJsonObject
 
 object MarkupParser {
     private val markupRegex =
-        Regex("\\[([\\w\\s\\-().'!?]+)(\\|(\\w+)(::([&=\\w:/\\\\.\\-_]+))?)?]")
+        Regex("\\[([\\w\\s\\-().'!?]+)(\\|(\\w+)(::([&=\\w:/\\\\.\\-_?]+))?)?]")
     private val lineBreakRegex = Regex("[<\\[]br[>\\]]")
     private val middotRegex = Regex("&middot;")
     private val autoSubmitPayloadRegex = Regex("(\\w+)=(\\w+)")
