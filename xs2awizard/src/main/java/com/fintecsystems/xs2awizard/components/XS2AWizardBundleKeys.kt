e611ad74eb2e1e090cede994cf128ff14665be25
Maik Mursall <mmursall@fintecsystems.com>
2022-12-02 15:47:54 +0100

    Added XS2AWizardBundleKeys

---
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/components/XS2AWizardBundleKeys.kt
@@ -0,0 +1,15 @@
+package com.fintecsystems.xs2awizard.components
+
+object XS2AWizardBundleKeys {
+    const val bundleName = "xs2aWizardConfig"
+
+    const val sessionKey = "sessionKey"
+    const val backendURL = "backendURL"
+    const val language = "language"
+    const val theme = "theme"
+    const val enableScroll = "enableScroll"
+    const val enableBackButton = "enableBackButton"
+    const val enableAutomaticRetry = "enableAutomaticRetry"
+
+    const val currentWebViewUrl = "currentWebViewUrl"
+}
