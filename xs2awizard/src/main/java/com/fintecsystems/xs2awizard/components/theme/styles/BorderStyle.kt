96a819772f397fba09ae75433c233ef2e4249e74
Maik Mursall <mmursall@fintecsystems.com>
2023-06-13 09:08:00 +0200

    Added secondary constructor for BorderStyle

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/components/theme/styles/BorderStyle.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/components/theme/styles/BorderStyle.kt
@@ -2,16 +2,35 @@ package com.fintecsystems.xs2awizard.components.theme.styles
 
 import android.os.Parcelable
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.fintecsystems.xs2awizard.components.theme.interop.XS2AColor
+import com.fintecsystems.xs2awizard.helper.DpParceler
 import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.TypeParceler
 
 /**
  * Border style definition.
  */
 @Immutable
 @Parcelize
+@TypeParceler<Dp, DpParceler>()
 data class BorderStyle(
-    val color: XS2AColor,
-    val width: Float,
-    val radius: Float
-) : Parcelable
+    val color: XS2AColor = XS2AColor.Unspecified,
+    val width: Dp = Dp.Unspecified,
+    val radius: Dp = Dp.Unspecified
+) : Parcelable {
+    /**
+     * Secondary constructor for non-compose projects.
+     */
+    @Suppress("unused")
+    constructor(
+        color: XS2AColor = XS2AColor.Unspecified,
+        width: Int? = null,
+        radius: Int? = null
+    ) : this(
+        color,
+        width?.dp ?: Dp.Unspecified,
+        radius?.dp ?: Dp.Unspecified
+    )
+}
