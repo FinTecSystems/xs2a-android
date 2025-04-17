54be3e93ae939441e058a618296ebc34e4e15a6a
Maik Mursall <maik.mursall@compeople.com>
2025-04-17 14:51:57 +0200

    Migrated SelectLine to ExposedDropdownMenu without LabelledContainer

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/SelectLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/SelectLine.kt
@@ -1,28 +1,21 @@
 package com.fintecsystems.xs2awizard.form.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
-import androidx.compose.ui.window.PopupProperties
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
 import com.fintecsystems.xs2awizard.form.SelectLineData
 import com.fintecsystems.xs2awizard.form.components.shared.FormText
@@ -39,6 +32,7 @@ import kotlinx.serialization.json.jsonPrimitive
  *
  * @param formData Data of this FormLine
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SelectLine(formData: SelectLineData) {
     // We need an reactive value, because formData.value is not reactive.
@@ -83,43 +77,25 @@ fun SelectLine(formData: SelectLineData) {
         mutableStateOf(false)
     }
 
-    // Workaround to let the dropdown have the same size as the TextField
-    var textFieldSize by remember { mutableStateOf(Size.Zero) }
-
-    // Used to clear the focus on select
-    val focusManager = LocalFocusManager.current
-
-    LabelledContainer(label = formData.label) {
+    ExposedDropdownMenuBox(
+        expanded = selectIsExpanded,
+        onExpandedChange = { selectIsExpanded = it },
+    ) {
         FormTextField(
+            modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable),
             readOnly = true,
             value = textFieldValue,
             onValueChange = { },
-            onGloballyPositioned = { textFieldSize = it.size.toSize() },
-            onFocusChanged = { selectIsExpanded = it.isFocused },
-            trailingIcon = {
-                Icon(
-                    Icons.Filled.ArrowDropDown, null,
-                    tint = XS2ATheme.CURRENT.textColor.value
-                )
-            }
+            singleLine = true,
+            label = formData.label,
+            // Replace tint = XS2ATheme.CURRENT.textColor.value with LocalContentColor
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = selectIsExpanded) },
         )
 
-        DropdownMenu(
+        ExposedDropdownMenu(
             expanded = selectIsExpanded,
-            onDismissRequest = {
-                focusManager.clearFocus()
-            },
-            properties = PopupProperties(
-                focusable = false,
-                dismissOnBackPress = true,
-                dismissOnClickOutside = true
-            ),
-            modifier = Modifier
-                .width(with(LocalDensity.current) { textFieldSize.width.toDp() })
-                .background(
-                    XS2ATheme.CURRENT.surfaceColor.value,
-                    XS2ATheme.CURRENT.inputShape.value,
-                )
+            onDismissRequest = { selectIsExpanded = false },
+            containerColor = XS2ATheme.CURRENT.surfaceColor.value
         ) {
             val optionsArray = when (formData.options) {
                 is JsonArray -> formData.options.toList()
@@ -127,23 +103,25 @@ fun SelectLine(formData: SelectLineData) {
                 else -> throw IllegalArgumentException()
             }
 
-            optionsArray.forEachIndexed { index, item ->
+            optionsArray.forEachIndexed { index, option ->
                 DropdownMenuItem(
-                    onClick = {
-                        setValue(index)
-                        focusManager.clearFocus()
-                    },
                     text = {
                         Column(
                             modifier = Modifier.padding(2.dp, 4.dp)
                         ) {
                             FormText(
-                                text = item.jsonPrimitive.content,
+                                text = option.jsonPrimitive.content,
                                 style = MaterialTheme.typography.titleMedium,
                                 maxLines = 1,
                             )
                         }
-                    })
+                    },
+                    onClick = {
+                        setValue(index)
+                        selectIsExpanded = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                )
             }
         }
     }
