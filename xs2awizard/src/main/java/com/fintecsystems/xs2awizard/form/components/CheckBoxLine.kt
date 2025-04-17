e45264ee70e78462e5d56bb5c06b31ad0ad39c4e
Maik Mursall <maik.mursall@compeople.com>
2025-04-17 17:22:47 +0200

    Migrated CheckBoxLine to Material3 convention

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/CheckBoxLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/CheckBoxLine.kt
@@ -1,24 +1,20 @@
 package com.fintecsystems.xs2awizard.form.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.fintecsystems.xs2awizard.components.XS2AWizardViewModel
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
@@ -44,12 +40,6 @@ fun CheckBoxLine(formData: CheckBoxLineData, viewModel: XS2AWizardViewModel) {
         )
     }
 
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-
-    val localFocusManager = LocalFocusManager.current
-
     val enabled = formData.disabled == false
 
     /**
@@ -61,50 +51,34 @@ fun CheckBoxLine(formData: CheckBoxLineData, viewModel: XS2AWizardViewModel) {
         checkBoxValue = newValue
         // Update formData.value as well
         formData.value = JsonPrimitive(newValue)
-
-        localFocusManager.clearFocus()
     }
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .offset((-14).dp, 0.dp)
-            .clickable(
-                interactionSource = interactionSource,
-                indication = ripple(),
+            .toggleable(
+                value = checkBoxValue,
                 enabled = enabled,
-                onClick = {
-                    onCheckedChange(!checkBoxValue)
-                }),
+                onValueChange = { onCheckedChange(!checkBoxValue) },
+                role = Role.Checkbox
+            ),
+        horizontalArrangement = Arrangement.spacedBy(14.dp)
     ) {
-
-        CompositionLocalProvider(LocalRippleConfiguration provides null) {
-            Checkbox(
-                checked = checkBoxValue,
-                onCheckedChange = ::onCheckedChange,
-                interactionSource = interactionSource,
-                enabled = enabled,
-                colors = CheckboxDefaults.colors(
-                    checkedColor = XS2ATheme.CURRENT.tintColor.value,
-                    uncheckedColor = XS2ATheme.CURRENT.unselectedColor.value,
-                    checkmarkColor = XS2ATheme.CURRENT.onTintColor.value
-                )
+        Checkbox(
+            checked = checkBoxValue,
+            onCheckedChange = null,
+            enabled = enabled,
+            colors = CheckboxDefaults.colors(
+                checkedColor = XS2ATheme.CURRENT.tintColor.value,
+                uncheckedColor = XS2ATheme.CURRENT.unselectedColor.value,
+                checkmarkColor = XS2ATheme.CURRENT.onTintColor.value
             )
-        }
+        )
 
         if (!formData.label.isNullOrEmpty()) {
             val parseResult = MarkupParser.parseMarkupText(formData.label)
 
             FormText(
-                modifier = Modifier
-                    .clickable(
-                        enabled = enabled,
-                        interactionSource = interactionSource,
-                        indication = null,
-                        onClick = {
-                            onCheckedChange(!checkBoxValue)
-                        }
-                    ),
                 parseResult = parseResult,
                 style = MaterialTheme.typography.bodyMedium.copy(
                     color = if (enabled) XS2ATheme.CURRENT.textColor.value else XS2ATheme.CURRENT.disabledColor.value
