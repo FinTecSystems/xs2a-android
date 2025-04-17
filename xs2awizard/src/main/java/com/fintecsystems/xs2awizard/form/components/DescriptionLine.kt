c1b6530f2afd697d935636df30f76193574629ee
Maik Mursall <maik.mursall@compeople.com>
2025-04-18 00:57:40 +0200

    Fixed Link focus jumping

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/DescriptionLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/DescriptionLine.kt
@@ -1,10 +1,12 @@
 package com.fintecsystems.xs2awizard.form.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.semantics
 import com.fintecsystems.xs2awizard.components.XS2AWizardViewModel
 import com.fintecsystems.xs2awizard.form.DescriptionLineData
 import com.fintecsystems.xs2awizard.form.components.shared.FormText
@@ -18,18 +20,25 @@ import com.fintecsystems.xs2awizard.helper.MarkupParser
  */
 @Composable
 fun DescriptionLine(formData: DescriptionLineData, viewModel: XS2AWizardViewModel) {
-    if (formData.text?.isNotEmpty() == true) {
-        val parseResult = MarkupParser.parseMarkupText(formData.text)
-        val localFocusManager = LocalFocusManager.current
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .semantics(true) {} // FIXME: If we don't do this and there is a LinkAnnotation within the text,
+                                //        the focus will be all over the place. If we find a better solution for this,
+                                //        then we can remove it.
+    ) {
+        if (formData.text?.isNotEmpty() == true) {
+            val parseResult = MarkupParser.parseMarkupText(formData.text)
+            val localFocusManager = LocalFocusManager.current
 
-        FormText(
-            modifier = Modifier.fillMaxWidth(),
-            parseResult = parseResult,
-            style = MaterialTheme.typography.bodyMedium,
-            onLinkAnnotationClick = {
-                viewModel.handleLinkAnnotationClick(it)
-                localFocusManager.clearFocus()
-            }
-        )
+            FormText(
+                modifier = Modifier.fillMaxWidth(),
+                parseResult = parseResult,
+                style = MaterialTheme.typography.bodyMedium,
+                onLinkAnnotationClick = {
+                    viewModel.handleLinkAnnotationClick(it)
+                    localFocusManager.clearFocus()
+                }
+            )
+        }
     }
 }
