07096624d1ce1e5d1dcf31eb2a93af625b17bd37
Maik Mursall <maik.mursall@compeople.com>
2025-05-08 15:31:33 +0200

    Migrated RadioLine away from LabelledContainer

--- xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/RadioLine.kt
+++ xs2awizard/src/main/java/com/fintecsystems/xs2awizard/form/components/RadioLine.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.runtime.Composable
@@ -16,6 +17,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.invisibleToUser
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.fintecsystems.xs2awizard.components.theme.XS2ATheme
 import com.fintecsystems.xs2awizard.form.RadioLineData
@@ -32,7 +36,7 @@ import kotlinx.serialization.json.jsonPrimitive
  * @param selected Selection state.
  * @param onClick OnClick callback.
  * @param label Label of the RadioButton
- * @param disabled Disabled state of the Button
+ * @param enabled Enabled state of the Button
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -95,26 +99,40 @@ fun RadioLine(formData: RadioLineData) {
         formData.value = JsonPrimitive(newValue)
     }
 
-    LabelledContainer(label = formData.label) {
-        Column(
-            modifier = Modifier.selectableGroup(),
-            verticalArrangement = Arrangement.spacedBy(5.dp)
-        ) {
-            formData.options.forEachIndexed { index, radioElement ->
-                if (radioElement is JsonPrimitive) {
-                    LabelledRadioButton(
-                        selected = index == selectedValue,
-                        onClick = { onSelectedChange(index) },
-                        label = radioElement.content
-                    )
-                } else {
-                    LabelledRadioButton(
-                        selected = index == selectedValue,
-                        onClick = { onSelectedChange(index) },
-                        label = radioElement.jsonObject["label"]?.jsonPrimitive?.content!!,
-                        enabled = radioElement.jsonObject["disabled"]?.jsonPrimitive?.booleanOrNull != true
-                    )
+    Column(
+        modifier = Modifier
+            .selectableGroup()
+            .semantics {
+                formData.label?.let {
+                    contentDescription = it
                 }
+            },
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
+        formData.label?.let {
+            FormText(
+                text = it,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.semantics {
+                    invisibleToUser()
+                }
+            )
+        }
+
+        formData.options.forEachIndexed { index, radioElement ->
+            if (radioElement is JsonPrimitive) {
+                LabelledRadioButton(
+                    selected = index == selectedValue,
+                    onClick = { onSelectedChange(index) },
+                    label = radioElement.content
+                )
+            } else {
+                LabelledRadioButton(
+                    selected = index == selectedValue,
+                    onClick = { onSelectedChange(index) },
+                    label = radioElement.jsonObject["label"]?.jsonPrimitive?.content!!,
+                    enabled = radioElement.jsonObject["disabled"]?.jsonPrimitive?.booleanOrNull != true
+                )
             }
         }
     }
