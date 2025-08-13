5e9e446508107007c532bb500751da95c7b816c8
Maik Mursall <maik.mursall@compeople.com>
2025-08-13 12:04:04 +0200

    Added kotlin-parcelize-runtime as an api dependency

--- xs2awizard/build.gradle.kts
+++ xs2awizard/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.aesEverywhere)
 
+    api(libs.kotlin.parcelizeRuntime)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
